86670dfbac99f7d4e8b16dd0ac624e492f9dbd13
Matt Williams <m@technovangelist.com>
2024-09-17 16:26:59 -0700

    line got moved somehow
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- 2024-09-10-buildrag/python/importdocs.py
+++ 2024-09-10-buildrag/python/importdocs.py
@@ -1,12 +1,17 @@
 import chromadb
 from functions import readtextfiles, chunksplitter, getembedding
 
+
+
+
 chromaclient = chromadb.HttpClient(host="localhost", port=8000)
 textdocspath = "../../scripts"
 text_data = readtextfiles(textdocspath)
+
+collection = chromaclient.get_or_create_collection(name="buildragwithpython", metadata={"hnsw:space": "cosine"}  )
 if any(collection.name == collectionname for collection in chromaclient.list_collections()):
   chromaclient.delete_collection("buildragwithpython")
-collection = chromaclient.get_or_create_collection(name="buildragwithpython", metadata={"hnsw:space": "cosine"}  )
+
 for filename, text in text_data.items():
   chunks = chunksplitter(text)
   embeds = getembedding(chunks)
